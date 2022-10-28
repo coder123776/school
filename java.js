45be53560afac183e573aeb4360a3b6e397214ad
coder123776 <114562702+coder123776@users.noreply.github.com>
2022-10-28 14:16:28 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,32 @@
+function changeColor(){
+    console.log("it worked");
+}
+//changeColor();
+
+function changeColor(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer);
+    alert(answer);
+}
+//Two(8, 15);
+
+function calculate(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer);
+    alert(answer);
+}
+//calculate(25, 15);
+
+function multiplyByFive(num, num2){
+    let answer = num * num2;
+    console.log(answer);
+    alert(answer);
+}
+//multiplyByFive(7, 5);
+
+function calculateMinutes(num, num2){
+    let answer = num / num2;
+    console.log(answer);
+    alert(answer);
+}
+//calculateMinutes(3600, 60);
