c02b3f78109a8030fd068c9e3174783846a25d9a
coder123776 <114562702+coder123776@users.noreply.github.com>
2022-11-02 13:53:38 +0100

    Update opdr7.js

--- opdr7.js
+++ opdr7.js
@@ -1,12 +1,15 @@
-var budget = 100;
+function Geld()
+{
+var budget = 10;
 var product = 60;
 
 if (budget >60) {
     test = 'U heeft genoeg geld!';
     console.log('U heeft genoeg geld!');
+    document.getElementById('test').innerHTML = test;
 } else{
     test = 'Helaas heeft u, te weinig geld';
     console.log('Helaas heeft u, te weinig geld');
+    document.getElementById('test').innerHTML = test;
 }
-
-document.getElementById('test').innerHTML = test;
+}
